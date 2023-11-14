484bb0ff4dec3bd444f6a4d1106345fcaf924d8a
UdanaIsiwari <it20163372@my.sliit.lk>
2023-11-14 15:38:50 +0530

    fix issues

--- src/components/Craftsmanship/CraftsmanshipLeft.tsx
+++ src/components/Craftsmanship/CraftsmanshipLeft.tsx
@@ -9,13 +9,11 @@ const CraftsmanshipLeft = ({changeOrder }: any) => {
       className="scroll-mt-14 post grid grid-cols-1 lg:px-[55px]"
     >
       <div className="flex flex-col">
-        <div className="flex flex-row">
-          <h1 className="text-[52px] text-black  font-semibold leading-10">
+          <h1 className="text-[40px] lg:text-[52px] text-black  font-semibold leading-10 px-[30px] lg:px-[0px]">
             Premium Craftsmanship
           </h1>
-        </div>
 
-        <div className="flex-1 p-12 px-20 flex flex-row justify-center md:col-span-2 text-center md:text-left h-full w-full">
+        <div className="flex-1 p-12 lg:px-20 flex flex-col lg:flex-row justify-center md:col-span-2 text-center md:text-left h-full w-full">
           <div
             className={`md:col-span-3 h-[40vh] w-full rounded-2xl ${
               changeOrder ? "order-2" : ""
@@ -29,7 +27,7 @@ const CraftsmanshipLeft = ({changeOrder }: any) => {
               width={1250}
             />
           </div>
-          <div className="flex mt-5 justify-center max-w-50ch max-w-screen-ch">
+          <div className="flex lg:mt-5 justify-center max-w-50ch max-w-screen-ch">
             <p className="text-[1.0rem] text-justify w-[80%]  font-barlow text-black tracking-0.5px">
               Dynamax RV Caravans take pride in delivering the utmost quality
               workmanship. With a skilled workforce boasting extensive
