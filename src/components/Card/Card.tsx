27b3e0e32ec0a54b80637571f14ec4bf88a041ce
UdanaIsiwari <it20163372@my.sliit.lk>
2023-11-14 13:41:13 +0530

    fixes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,43 +2,58 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 export default function Card({ image, name, gallery }: any) {
-  const navigateToContactUs = () => {
+ 
+
+  const learnmore = () => {
     window.location.href = "/#contact-us";
   };
+
   return (
-    <Link href={{ pathname: "/grid", query: { gallery: JSON.stringify(gallery) } }} passHref>
-    <div className="w-full h-[45vh] flex flex-col overflow-hidden rounded-2xl relative ">
-      <Image
-        src={image}
-        alt="Hero Image"
-        fill
-        className="object-cover w-full h-full "
-      />
-      <div className="absolute bottom-4 left-4 rounded-2xl  bg-white backdrop-blur-md p-3 flex flex-col">
-        <div className="flex items-center justify-between">
-          <h3 className="text-[1.5rem] text-black font-[700]">{name}</h3>
-          {/* <h3 className="text-[1.1rem] font-[700]">$90,400</h3> */}
-        </div>
-        <div className="flex items-center justify-between">
-          {/* <h3 className="text-[1rem] font-[400]">Semi Off Road</h3>
+    // <Link
+    //   href={{ pathname: "/grid", query: { gallery: JSON.stringify(gallery) } }}
+    //   passHref
+    // >
+      <div className="w-full h-[45vh] flex flex-col overflow-hidden rounded-2xl relative ">
+        <Link
+      href={{ pathname: "/grid", query: { gallery: JSON.stringify(gallery) } }}
+      passHref
+    >
+        <Image
+          src={image}
+          alt="Hero Image"
+          fill
+          className="object-cover w-full h-full "
+        />
+        </Link>
+        <div className="absolute bottom-4 left-4 rounded-2xl  bg-white backdrop-blur-md p-3 flex flex-col">
+          <div className="flex items-center justify-between">
+            <h3 className="text-[1.5rem] text-black font-[700]">{name}</h3>
+            {/* <h3 className="text-[1.1rem] font-[700]">$90,400</h3> */}
+          </div>
+          <div className="flex items-center justify-between">
+            {/* <h3 className="text-[1rem] font-[400]">Semi Off Road</h3>
           <h3 className="text-[1rem] font-[400]">$112,000</h3> */}
-        </div>
-        {/* <Link
+          </div>
+          {/* <Link
           href="/product"
           className="text-[#B90000]  text-[1.1rem] text-center py-[.25rem] px-[1.5rem] rounded-[1.2rem] border-[#b90000] border-solid border-2 hover:bg-[#b90000] hover:text-[#fff]"
         >
           View
         </Link> */}
-      </div>
+        </div>
 
-      <div className="absolute top-4 left-4 rounded-2xl  bg-white bg-opacity-70 backdrop-blur-md p-2">
-        
-      <button className="flex items-center justify-between" onClick={navigateToContactUs}>
-          <h5 className="text-[0.8rem] text-black font-[700]">LEARN DETAILS</h5>
-        </button>
-        
+        <Link
+          href="/#contact-us"
+          // onClick={learnmore}
+          className="absolute top-4 left-4 rounded-2xl  bg-white bg-opacity-70 backdrop-blur-md p-2"
+        >
+          <div className="flex items-center justify-between">
+            <h5 className="text-[0.8rem] text-black font-[700]">
+              LEARN DETAILS
+            </h5>
+          </div>
+        </Link>
       </div>
-    </div>
-    </Link>
+    // </Link>
   );
 }
